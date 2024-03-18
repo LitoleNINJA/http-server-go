f51db2ed1b9442df90423a0c2cf2de85fd4e423d
Ritwik <sritwik03@gmail.com>
2024-03-18 23:44:50 +0530

    tsak 2

--- app/server.go
+++ app/server.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+type httpReq struct {
+	method  string
+	path    string
+	version string
+	headers map[string]string
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -30,17 +38,49 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
 		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		n, err := conn.Read(buf)
 		if err != nil {
 			fmt.Println("Error reading: ", err.Error())
 			return
 		}
-		fmt.Printf("\nReceived data: %s", string(buf))
+		fmt.Printf("\nRecieved: %s Bytes: %d\n", string(buf[:n]), n)
 
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		req, err := parseRequest(string(buf))
+		var res []byte
+		if req.path == "/" {
+			res = []byte("HTTP/1.1 200 OK\r\n\r\n")
+		} else {
+			res = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+		}
+
+		_, err = conn.Write(res)
 		if err != nil {
 			fmt.Println("Error writing: ", err.Error())
 			return
 		}
 	}
+}
+
+func parseRequest(req string) (httpReq, error) {
+	a := strings.Split(req, "\r\n")
+
+	method := strings.TrimSpace(strings.Split(a[0], " ")[0])
+	path := strings.TrimSpace(strings.Split(a[0], " ")[1])
+	version := strings.TrimSpace(strings.Split(a[0], " ")[2])
+	headers := make(map[string]string)
+	for i := 1; i < len(a); i++ {
+		if a[i] == "" {
+			break
+		}
+		h := strings.Split(a[i], ":")
+		headers[strings.TrimSpace(h[0])] = strings.TrimSpace(h[1])
+	}
+	// fmt.Printf("Method: %s\nPath: %s\nVersion: %s\nHeaders: %v\n", method, path, version, headers)
+
+	return httpReq{
+		method:  method,
+		path:    path,
+		version: version,
+		headers: headers,
+	}, nil
 }
