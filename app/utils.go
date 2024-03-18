a3944fb6f48b67a808cad573243693ba74ee58ac
Ritwik <sritwik03@gmail.com>
2024-03-19 00:42:43 +0530

    task 6

--- app/utils.go
+++ app/utils.go
@@ -1,9 +1,11 @@
 package main
 
+// HasPrefix reports whether s starts with prefix.
 func HasPrefix(s, prefix string) bool {
 	return len(s) >= len(prefix) && s[0:len(prefix)] == prefix
 }
 
+// CutPrefix returns s without the provided prefix, if present.
 func CutPrefix(s, prefix string) (after string, found bool) {
 	if !HasPrefix(s, prefix) {
 		return s, false
