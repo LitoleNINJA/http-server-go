3a02b24026f434098ca969a234a7cfc89562bdcd
Ritwik <sritwik03@gmail.com>
2024-03-19 11:56:18 +0530

    task 7

--- app/server.go
+++ app/server.go
@@ -60,69 +60,68 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	for {
-		buf := make([]byte, 1024)
-		n, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading: ", err.Error())
-			return
-		}
-		fmt.Printf("\nRecieved: %s Bytes: %d\n", string(buf[:n]), n)
 
-		req, err := parseRequest(string(buf))
-		if err != nil {
-			fmt.Println("Error parsing request: ", err.Error())
-			return
-		}
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		return
+	}
+	fmt.Printf("\nRecieved: %s Bytes: %d\n", string(buf[:n]), n)
 
-		var res httpRes
-		res.version = req.version
-		if req.path == "/" {
-			res.status = "200 OK"
-		} else if pre, ok := CutPrefix(req.path, "/echo/"); ok {
-			res.status = "200 OK"
-			res.headers = map[string]string{
-				"Content-Type":   "text/plain",
-				"Content-Length": fmt.Sprintf("%d", len(pre)),
+	req, err := parseRequest(string(buf))
+	if err != nil {
+		fmt.Println("Error parsing request: ", err.Error())
+		return
+	}
+
+	var res httpRes
+	res.version = req.version
+	if req.path == "/" {
+		res.status = "200 OK"
+	} else if pre, ok := CutPrefix(req.path, "/echo/"); ok {
+		res.status = "200 OK"
+		res.headers = map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(pre)),
+		}
+		res.body = pre
+	} else if strings.HasPrefix(req.path, "/user-agent") {
+		res.status = "200 OK"
+		res.body = req.headers["User-Agent"]
+		res.headers = map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(res.body)),
+		}
+	} else if filename, ok := CutPrefix(req.path, "/files/"); ok {
+		file, err := os.Open(*dir + "/" + filename)
+		if err != nil {
+			res.status = "404 Not Found"
+		} else {
+			data, err := io.ReadAll(file)
+			if err != nil {
+				fmt.Println("Error reading file: ", err.Error())
+				return
 			}
-			res.body = pre
-		} else if strings.HasPrefix(req.path, "/user-agent") {
 			res.status = "200 OK"
-			res.body = req.headers["User-Agent"]
+			res.body = string(data)
 			res.headers = map[string]string{
-				"Content-Type":   "text/plain",
+				"Content-Type":   "application/octet-stream",
 				"Content-Length": fmt.Sprintf("%d", len(res.body)),
 			}
-		} else if filename, ok := CutPrefix(req.path, "/files/"); ok {
-			file, err := os.Open(*dir + "/" + filename)
-			if err != nil {
-				res.status = "404 Not Found"
-				res.body = "File not found"
-			} else {
-				data, err := io.ReadAll(file)
-				if err != nil {
-					fmt.Println("Error reading file: ", err.Error())
-					return
-				}
-				res.status = "200 OK"
-				res.body = string(data)
-				res.headers = map[string]string{
-					"Content-Type":   "application/octet-stream",
-					"Content-Length": fmt.Sprintf("%d", len(res.body)),
-				}
-			}
-			file.Close()
-		} else {
-			res.status = "404 Not Found"
 		}
-
-		_, err = conn.Write(res.encode())
-		if err != nil {
-			fmt.Println("Error writing: ", err.Error())
-			return
-		}
-		fmt.Println("Sent: ", res)
+		file.Close()
+	} else {
+		res.status = "404 Not Found"
 	}
+
+	_, err = conn.Write(res.encode())
+	if err != nil {
+		fmt.Println("Error writing: ", err.Error())
+		return
+	}
+	fmt.Println("Sent: ", res)
+
 }
 
 func parseRequest(req string) (httpReq, error) {
