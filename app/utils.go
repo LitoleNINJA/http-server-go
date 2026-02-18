2434e127ec4216d1bf5c68208f78471508b96dd5
LitoleNINJA <sritwik2@gmail.com>
2026-02-18 10:19:22 +0000

    codecrafters submit [skip ci]

--- app/utils.go
+++ app/utils.go
@@ -5,12 +5,10 @@ import (
 	"bytes"
 )
 
-// HasPrefix reports whether s starts with prefix.
 func HasPrefix(s, prefix string) bool {
 	return len(s) >= len(prefix) && s[0:len(prefix)] == prefix
 }
 
-// CutPrefix returns s without the provided prefix, if present.
 func CutPrefix(s, prefix string) (after string, found bool) {
 	if !HasPrefix(s, prefix) {
 		return s, false
